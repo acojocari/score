3989ec66ae493eb304b8703d584720ec25c57be0
acojocari <anatolcojocari@gmail.com>
2021-11-21 17:07:26 +0100

    Add the header's app

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  Dimensions
+} from 'react-native';
 
 export default function App() {
+
+ // console.log(Dimensions.get("screen"));
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
+    <SafeAreaView style={mainStyle.container}>
+      <View style={headerStyle.container}>
+        <View>
+          <Text style ={headerStyle.text}>SCORE GAME</Text>
+        </View>
+        <View>
+          <Text style ={headerStyle.text}>       Players: 3</Text>
+        </View>
+
+      </View>
+
+      <View style={bodyStyle.container}>
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
+const headerStyle = StyleSheet.create({
+  container: {
+    width:"100%",
+    height:"10%",
+    backgroundColor: '#8063FA',
+    justifyContent: "center",
+    alignItems: "center",
+    top:10,
+    flexDirection: "row"
+  },
+  text: {
+    color: "#000",
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
+
+const bodyStyle = StyleSheet.create({
+  container: {
+    width:"100%",
+    height:"50%",
+    backgroundColor: '#fff'
+  },
+});
+
+const mainStyle = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:Platform.OS ==="android" ? 30 : 0,
   },
 });
